cdd16caef85e3bcbf7eeb15157a19457fa5f20b1
pepitogalvis <jose.galvis@gmail.com>
2022-10-26 22:15:39 -0500

    Commit del 26/10/2022 a las 10:14 pm, Pantalla de inicio de sesión.

--- app/src/main/java/com/example/unleeg8/View/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/unleeg8/View/ui/activities/MainActivity.kt
@@ -1,28 +1,40 @@
-package com.example.unleeg8
+package com.example.unleeg8.View.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.os.Handler
+import android.os.Looper
+import com.example.unleeg8.R
+import com.example.unleeg8.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Las siguientes dos lineas son para asociar la activity a un layout
+        // es igual que el setContentView(R.layout.activity_main) pero mejor
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val botonEnviar: Button = findViewById(R.id.btnEnviar)
-        botonEnviar.setOnClickListener {
-            /*Toast.makeText(this, "auch me clicaste",
-            Toast.LENGTH_LONG).show()*/
-            val texto: TextView = findViewById(R.id.textHellow)
-            texto.text=aleatorio(100).toString()
-        }
+        //configuracion para correcr la animacion en el layout activity_main
 
+        binding.animationView.setAnimation(R.raw.pet)
+        binding.animationView.playAnimation()
+
+        // configuracion para que la transicion entre la animacion
+        // y la siguiente activity se realice tras 4 seg
+
+        handler=Handler(Looper.myLooper()!!)
+
+        handler.postDelayed({
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
     }
 
-    fun aleatorio (max:Int):Int{
 
-        return (1..max).random()
-    }
 }
---
+++ app/src/main/java/com/example/unleeg8/View/ui/activities/login.kt
@@ -0,0 +1,15 @@
+package com.example.unleeg8.View.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.unleeg8.R
+import com.example.unleeg8.databinding.ActivityMainBinding
+
+class login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
