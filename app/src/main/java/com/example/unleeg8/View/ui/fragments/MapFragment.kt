1b7e0effe90fef7e467b39de9a0b80cf7dd14555
pepitogalvis <jose.galvis@gmail.com>
2022-11-17 19:59:05 -0500

    Commit del 17/11/2022 7:58 PM
    Adicion de funciones de ubicacion en tiempo real.
    Cambio de ubicacion de oficinas de la empresa.

--- app/src/main/java/com/example/unleeg8/View/ui/fragments/MapFragment.kt
+++ app/src/main/java/com/example/unleeg8/View/ui/fragments/MapFragment.kt
@@ -1,9 +1,14 @@
 package com.example.unleeg8.View.ui.fragments
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.unleeg8.R
 import com.google.android.gms.maps.GoogleMap
@@ -11,16 +16,15 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory
-import org.osmdroid.util.GeoPoint
-import org.osmdroid.views.MapView
-import org.osmdroid.views.overlay.Marker
+
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
     lateinit var googleMap: GoogleMap
 
-    lateinit var mapView: MapView
+    companion object {
+        const val REQUEST_CODE_LOCATION=0
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,11 +44,38 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
 
     override fun onMapReady(map: GoogleMap) {
-        val colombia = LatLng(5.0700275, -75.513817)
+        val colombia = LatLng(4.89975, -74.03957)
         map?.let {
+            this.googleMap = it
             map.addMarker(MarkerOptions().position(colombia))
         }
+        enableLocation()
     }
+
+    fun isLocationPermissionGrated()=ContextCompat.checkSelfPermission(
+        this.requireContext(),android.Manifest.permission.ACCESS_FINE_LOCATION
+    )==PackageManager.PERMISSION_GRANTED
+
+    fun requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this.requireActivity(),
+            android.Manifest.permission.ACCESS_FINE_LOCATION)){
+            Toast.makeText(this.context,"Activar Permisos de Ubicación",Toast.LENGTH_LONG).show()
+        }else {
+            ActivityCompat.requestPermissions(this.requireActivity(), arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+            com.example.unleeg8.View.ui.fragments.MapFragment.Companion.REQUEST_CODE_LOCATION)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun enableLocation(){
+        if(!::googleMap.isInitialized)return
+        if(isLocationPermissionGrated()){
+            googleMap.isMyLocationEnabled=true
+        }else{
+            requestLocationPermission()
+        }
+    }
+
 
 
 }
